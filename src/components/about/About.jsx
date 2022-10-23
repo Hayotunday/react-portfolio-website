54d42d5b2f904327781cca96f997d76722a48efe
Hayotunday <idowudanielayotunde@gmail.com>
2022-10-23 16:36:10 +0100

    First commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jfif";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_image" />
+              <h5>Experience</h5>
+              <small>3+ Years Working experience</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_image" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_image" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            distinctio nobis perferendis odio. Omnis magni reprehenderit
+            repudiandae incidunt rem beatae architecto dolore, blanditiis, ipsam
+            illum ipsa odit quod ad rerum.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
